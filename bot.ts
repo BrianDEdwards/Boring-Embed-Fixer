e699daac57c02002977074c4b6a17fc7f7c9f590
Brian Edwards <brian.dedwards@gmail.com>
2024-01-07 15:18:45 +0000

    Fix regex to only respond with the link

--- bot.ts
+++ bot.ts
@@ -2,13 +2,13 @@ import { match } from 'assert';
 import CONFIG from './config.json';
 import * as Discord from 'discord.js';
 
-const TWITTER_MATCHER = /(.*)(twitter|x\.com)(\/.+\/status\/.*)/g;
+const TWITTER_MATCHER = /(.*)(https\:\/\/)(twitter|x\.com)(\/.+\/status\/[\w\/]*)(.*)/g;
 const TWITTER_FIX: string = 'vxtwitter.com';
 
-const TIK_TOK_MATCHER = /(.*)(tiktok.com)(\/t\/.*)/g;
+const TIK_TOK_MATCHER = /(.*)(https:\/\/)(www.tiktok.com)(\/t\/[\w\/]*)(.*)/g;
 const TIK_TOK_FIX = 'vxtiktok.com';
 
-const INSTA_MATCHER = /(.*)(instagram.com)(\/p\/.*)/g;
+const INSTA_MATCHER = /(.*)(https:\/\/)(www.instagram.com)(\/p\/[\w\/]*)(.*)/g;
 const INSTA_FIX = 'ddinstagram.com';
 
 const client = new Discord.Client();
@@ -26,15 +26,15 @@ client.on('message', async message => {
     let newMessage = message.content;
 
     if(TWITTER_MATCHER.test(message.content)){
-        newMessage = message.content.replace(TWITTER_MATCHER, `$1${TWITTER_FIX}$3`);
+        newMessage = message.content.replace(TWITTER_MATCHER, `$2${TWITTER_FIX}$4`);
         replaced = true;
     }
     if(TIK_TOK_MATCHER.test(message.content)){
-        newMessage = message.content.replace(TIK_TOK_MATCHER, `$1${TIK_TOK_FIX}$3`);
+        newMessage = message.content.replace(TIK_TOK_MATCHER, `$2${TIK_TOK_FIX}$4`);
         replaced = true;
     }
     if(INSTA_MATCHER.test(message.content)){
-        newMessage = message.content.replace(INSTA_MATCHER, `$1${INSTA_FIX}$3`);
+        newMessage = message.content.replace(INSTA_MATCHER, `$2${INSTA_FIX}$4`);
         replaced = true;
     }
     
