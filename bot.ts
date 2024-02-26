42a4a4f7fd7d04af5e8c2551a13786d17132912b
Brian Edwards <brian.dedwards@gmail.com>
2024-02-26 03:10:23 +0000

    Fix twitter.com links not being fixed. Properly strip query string if trailing slash is missing

--- bot.ts
+++ bot.ts
@@ -3,15 +3,15 @@ import * as Discord from 'discord.js';
 
 const MATCHERS = [
     {
-        matcher: /(.*)(https\:\/\/)(twitter|x\.com)(\/.+\/status\/[\w\/]*)(.*)/,
+        matcher: /(.*)(https\:\/\/)(twitter\.com|x\.com)(\/.+\/status\/[\w]*)(.*)/,
         fix: 'vxtwitter.com'
     },
     {
-        matcher: /(.*)(https:\/\/)(www.tiktok.com)(\/t\/[\w\/]*)(.*)/,
+        matcher: /(.*)(https:\/\/)(www.tiktok.com)(\/t\/[\w]*)(.*)/,
         fix: 'vxtiktok.com'
     },
     {
-        matcher:  /(.*)(https:\/\/)(www.instagram.com)(\/p\/[\w\/]*)(.*)/,
+        matcher: /(.*)(https:\/\/)(www.instagram.com)(\/p\/[\w]*)(.*)/,
         fix: 'ddinstagram.com'
     }
 ]
@@ -25,24 +25,24 @@ client.once('ready', () => {
 client.login(CONFIG.secret);
 
 client.on('message', async message => {
-    if(message.author.bot) return;
+    if (message.author.bot) return;
 
     let links = [];
     MATCHERS.forEach(matcherGroup => {
         checkForLinks(matcherGroup, message.content, links);
     })
-    
-    if(links.length > 0) {
+
+    if (links.length > 0) {
         await message.channel.send(links.join(' '));
     }
 });
 
 
-function checkForLinks(matcherGroup: {matcher: RegExp, fix: string}, message: string, links: string[]){
+function checkForLinks(matcherGroup: { matcher: RegExp, fix: string }, message: string, links: string[]) {
     const matchedGroups = matcherGroup.matcher.exec(message);
 
-    if(matchedGroups == null) return;
-    else{
+    if (matchedGroups == null) return;
+    else {
         links.push(`${matchedGroups[2]}${matcherGroup.fix}${matchedGroups[4]}`);
         checkForLinks(matcherGroup, matchedGroups[5], links);
     }
